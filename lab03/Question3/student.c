55ff98bac8b79d09353870b7d5a92b67e6eb7340
NickDitmore <nickditmore@outlook.com>
2026-03-05 14:55:12 -0600

    Add files via upload

--- lab03/Question3/student.c
+++ lab03/Question3/student.c
@@ -13,6 +13,17 @@
 #include "student.h"
 
 bool hasCycle(struct ListNode *head) {
-    // TODO: implement
+    struct ListNode *slow = head;
+    struct ListNode *fast = head;
 
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;          // move 1 step
+        fast = fast->next->next;    // move 2 steps
+
+        if (slow == fast) {
+            return true;            // cycle found
+        }
+    }
+
+    return false;                   // no cycle
 }
