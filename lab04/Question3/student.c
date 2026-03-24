83933b65e34eb32ce7ded9f4d2dbf6d2cc8c41be
NickDitmore <nickditmore@outlook.com>
2026-03-23 21:47:11 -0500

    Add files via upload

--- lab04/Question3/student.c
+++ lab04/Question3/student.c
@@ -56,6 +56,22 @@ struct TreeNode {
 };
 
 
+int helper(struct TreeNode* node, int current) {
+    if (node == NULL) {
+        return 0;
+    }
+
+    current = current * 10 + node->val;
+
+    // if leaf node
+    if (node->left == NULL && node->right == NULL) {
+        return current;
+    }
+
+    return helper(node->left, current) +
+           helper(node->right, current);
+}
+
 int sumNumbers(struct TreeNode* root) {
-      // TODO: implement
+    return helper(root, 0);
 }
